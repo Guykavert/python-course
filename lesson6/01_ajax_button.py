51724dea60a49790476062a7cbe7cf7c5f4cfc09
Guykavert <guykavert@mail.ru>
2025-10-20 13:11:30 +0300

    Create 01_ajax_button.py

---
+++ lesson6/01_ajax_button.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def test_ajax_button():
+    driver = webdriver.Firefox()
+    
+    try:
+        driver.get("http://uitestingplayground.com/ajax")
+        time.sleep(3)
+        
+        button = driver.find_element(By.ID, "ajaxButton")
+        button.click()
+        
+        time.sleep(25)
+        element = driver.find_element(By.XPATH, "//*[contains(text(), 'Data loaded')]")
+        print(element.text)
+        
+    except Exception as e:
+        print("Ошибка:", e)
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    test_ajax_button()
